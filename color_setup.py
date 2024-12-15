0ac83e67a58fffc95ed83c9ac4966f30e399f97d
Ken Whitaker <kwhitaker@thinksharp.net>
2024-12-15 12:40:09 -0800

    Updated color_setup.py to work with my waveshare board.

--- color_setup.py
+++ color_setup.py
@@ -5,14 +5,21 @@
 # ILI9486 on Pi Pico
 # See DRIVERS.md for wiring details.
 
-from machine import Pin, SPI
+# from machine import Pin, SPI
+# import gc
+
+# from drivers.ili94xx.ili9486 import ILI9486 as SSD
+
+# pdc = Pin(8, Pin.OUT, value=0)
+# prst = Pin(9, Pin.OUT, value=1)
+# pcs = Pin(10, Pin.OUT, value=1)
+# spi = SPI(0, sck=Pin(6), mosi=Pin(7), miso=Pin(4), baudrate=30_000_000)
+# gc.collect()  # Precaution before instantiating framebuf
+# ssd = SSD(spi, pcs, pdc, prst)
+
+import machine
 import gc
+from drivers.epaper.pico_epaper_42_v2 import EPD as SSD  # V2 driver
 
-from drivers.ili94xx.ili9486 import ILI9486 as SSD
-
-pdc = Pin(8, Pin.OUT, value=0)
-prst = Pin(9, Pin.OUT, value=1)
-pcs = Pin(10, Pin.OUT, value=1)
-spi = SPI(0, sck=Pin(6), mosi=Pin(7), miso=Pin(4), baudrate=30_000_000)
-gc.collect()  # Precaution before instantiating framebuf
-ssd = SSD(spi, pcs, pdc, prst)
+gc.collect()  # Precaution before instantiating framebuf.
+ssd = SSD()  # Create a display instance based on a Pico in socket.
